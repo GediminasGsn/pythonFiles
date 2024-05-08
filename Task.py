784d8ce1cf249c03c4f9c3faccddbedd81e800e4
GediminasGsn <gasiunasgediminas@gmail.com>
2024-05-08 13:02:02 +0300

    ***

--- Task.py
+++ Task.py
@@ -6,6 +6,12 @@ red_counter = 0
 yellow_counter = 0
 green_counter = 0
 
+red_activeTime = 0
+yellow_activeTime = 0
+green_activeTime = 0
+
+green_time = 0
+
 
 # Open the file in read mode
 with open(r'C:\Users\Testuotojas\Downloads\Python_task_qdev_Jnr\task\data.txt', 'r') as file:
@@ -21,11 +27,23 @@ for line in data:
     # If 'Red' is '1', increment the counter
     if parsed_data['Red'] == '1':
         red_counter += 1
+        red_activeTime += int(parsed_data['TimeActive'])
     if parsed_data['Yellow'] == '1':
         yellow_counter +=1
+        yellow_activeTime += int(parsed_data['TimeActive'])
     if parsed_data['Green'] == '1':
         green_counter +=1
+        green_activeTime += int(parsed_data['TimeActive'])
+        green_time += int(parsed_data['Time'])
 print ("Times when all lights were active:")
 print(f"'Red' = {red_counter} ")
 print(f"'Yellow' = {yellow_counter} ")
 print(f"'Green' = {green_counter} ")
+print("-------------------------------------------------------------------------------------------")
+print("How much time the light was active:")
+print(f"'Red' = {red_activeTime} seconds.")
+print(f"'Yellow' = {yellow_activeTime} seconds.")
+print(f"'Green' = {green_activeTime} seconds.")
+print("-------------------------------------------------------------------------------------------")
+print("Time when green light was active:")
+print(f"'Green' = {green_time} seconds.")
